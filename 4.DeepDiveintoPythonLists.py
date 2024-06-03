facd2f1197d92fab8e2ccd8267673ef8d5165ad5
James Z <wuab@yahoo.com>
2024-06-03 14:56:37 -0700

    Update 4.DeepDiveintoPythonLists.py

--- 4.DeepDiveintoPythonLists.py
+++ 4.DeepDiveintoPythonLists.py
@@ -1,6 +1,4 @@
 # 4. Deep Dive into Python Lists
-# Objective: The aim of this assignment is to integrate various list operations and methods to solve a complex problem.
-# Problem Statement: You're organizing a school event, and you have lists containing student names, their grades, and the activities they're interested in.
 
 # Task 1: Given the lists:
 # students = ["John", "Doe", "Jane", "Smith"]
