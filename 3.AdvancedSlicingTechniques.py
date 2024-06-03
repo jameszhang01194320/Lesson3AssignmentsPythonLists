a285c5d3ea2e454aa6c42a2804825c17d7335e74
James Z <wuab@yahoo.com>
2024-06-03 14:57:16 -0700

    Update 3.AdvancedSlicingTechniques.py

--- 3.AdvancedSlicingTechniques.py
+++ 3.AdvancedSlicingTechniques.py
@@ -1,7 +1,4 @@
 # 3. Advanced Slicing Techniques
-# Objective: The aim of this assignment is to master the art of slicing in Python lists.
-# Problem Statement: You have a list of daily temperatures for a month, and you'd like to extract specific data from it.
-
 
 # Task 1: Given the list of temperatures:
 # temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
